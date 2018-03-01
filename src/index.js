62340514d662a3546ef3dc2ca87aeda408b738e4
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-03-01 17:56:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseBiggestFactor = 0, root=Math.floor(Math.sqrt(base))+1;
+  for(let i=root;i>0;i--)
+  {
+    if(base%i==0) {
+      if(base/i < i) {
+        baseBiggestFactor=i;break;
+      }
+      else {
+        baseBiggestFactor=base/i;break;
+      }
+    }
+  }
+
+  //аналогично предыдущей задаче
+  let count=0;
+  do{
+    number=Math.floor(number/baseBiggestFactor);
+    count+=number;
+  }
+  while(number>baseBiggestFactor-1)
+  return count;
 }
