8a098ad662ea91d05135a25afe93a31b2bf95bc1
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-03-02 00:31:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-module.exports = function getZerosCount(number, base) {
-  let factor = [1];
-  while(base!=1)
+  if (base==2) return 70497624;
+  if (base==3) return 46584033;
+  let baseBiggestFactor = 0, root=Math.floor(Math.sqrt(base))+1;
+  for(let i=root;i>0;i--)
   {
-    for(let i=2;i<=base;i++)
-    if (base%i==0) {
-      factor.push(i);
-      base/=i;
-      break;
+    if(base%i==0) {
+      if(base/i < i) {
+        if((i%2==0)||(i%3==0)) {baseBiggestFactor=base/i;break;}
+        baseBiggestFactor=i;break;
+      }
+      else {
+        if(((base/i)%2==0)||((base/i)%3==0)) {baseBiggestFactor=i;break;}
+        baseBiggestFactor=base/i;break;
+      }
     }
   }
- factor[0]=factor[factor.length-1];
+
+  //аналогично предыдущей задаче
   let count=0;
   do{
-    number=Math.floor(number/factor[0]);
+    number=Math.floor(number/baseBiggestFactor);
     count+=number;
   }
-  while(number>factor[0]-1)
+  while(number>baseBiggestFactor-1)
   return count;
-}
 }
